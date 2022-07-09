40296e1c68592c04bbf1fc037d6552d302071e94
kenkan <kenkan>
2022-07-10 02:15:01 +0700

    code original gue jangan hapus credit okey

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# spill-and-drink
-kalau banyak like bakal di kerjakan bot nya
+## SPILL AND DRINK BOT
+
+___
+Ini adalah repo original code pertama saya, jadi maklumin aja kalau banyak kekurangannya
+___
+
+# Deploy on Heroku
+<details>
+<summary><b> 🚀 Heroku Deployment</b></summary>
+<br>
+
+<h3 align="left">Klik Tombol di Bawah ini untuk Deploy di Heroku</h3>
+<p align="left"><a href="https://heroku.com/deploy?template=https://github.com/BukanDev/spill-and-drink"><img src="https://www.herokucdn.com/deploy/button.png" alt="Deploy to Heroku" target="_blank"/></a></p>
+</details>
+
+
+## Command
+
+```
+/spill - spill dulu
+/drink - minum dulu
+/donasi - donasi ke owner bot
+/request - request spill bikinan mu
+```
+
+## CREDIT
+- [KENKAN](https://github.com/kenkansaja) - DEV
+- [Dan](https://github.com/pyrogram/pyrogram) - Pyrogram
+
+## Group dan channel
+- [@musikkugroup](https://t.me/musikkugroup)
+- [@musikkuchannel](https://t.me/musikkuchannel)
+- [@PeimeSupportGroup](https://t.me/PrimeSupportGroup)
+- [@PrimeSupportChannel](https://t.me/PrimeSupportChannel)
+
+**KALAU CLONE ATAU FORK JANGAN HAPUS CREDIT YA JANCOK**
---
+++ kenkan.py
@@ -0,0 +1,85 @@
+# Copyright 2022 @BukanDev
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# https://github.com/BukanDev/spill-and-drink
+# limitations under the License.
+
+import os
+import sys
+from pyrogram import Client, filters
+from random import choice
+from requests import get
+from dotenv import load_dotenv
+load_dotenv()
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHANNEL = os.environ.get("CHANNEL")
+OWNER = os.environ.get("OWNER")
+LOG_CHAT = os.environ.get("LOG_CHAT")
+
+bot = Client("pornhub_bot",
+            api_id=6,
+            api_hash="eb06d4abfb49dc3eeb1aeb98ae0f581e",
+            bot_token=BOT_TOKEN)
+ 
+# atart text
+start = f"Selamat bermain dan selamat ter spill!\n\n Jangan lupa subs @{CHANNEL} dan contact @{OWNER} untuk info lainnya.\n\nNote : khusus RL bukan RP"
+
+# spill text
+spill = get("https://raw.githubusercontent.com/BukanDev/spill-and-drink/master/bahan/spill.json").json()
+
+# drink
+drink = get("https://raw.githubusercontent.com/BukanDev/spill-and-drink/master/bahan/drink.json").json()
+
+# donasi text
+donasi = f"Bagi yang punya duit penuh, atau berlebih bisa kali di transfer ke @{OWNER}"
+
+# help text
+helps = "/spill - spill dulu\n/drink - minum dulu\n/donasi - donasi ke owner bot\n/request - request spill bikinan mu"
+
+           
+@bot.on_message(filters.command("start") & filters.private & filters.group)
+async def start(bot, m):
+    await m.reply(start)
+    
+@bot.on_message(filters.command("help") & filters.private & filters.group)
+async def helps(bot, m):
+    await m.reply(helps)
+
+@bot.on_message(filters.command("spill") & filters.group, group=3)
+async def spill(bot, m):
+    await m.reply(choice(spill))
+    
+@bot.on_message(filters.command("drink") & filters.group, group=3)
+async def drink(bot, m):
+    await m.reply_photo(choice(drink))
+
+@bot.on_message(filters.command("donasi") & filters.group, group=3)
+async def donasi(bot, m):
+    await m.reply(donasi)
+    
+@bot.on_message(filters.command("request") & filters.group & filters.private)
+async def request(bot, m):
+    if len(m.command) < 2:
+         return await m.reply("contoh = `/request spill photo mantan kamu`")
+    mmk = m.command[1:]
+    kontol = " ".join(mmk)
+    await bot.copy(LOG_CHAT, message.chat.id, kontol)
+    await m.reply("Terimakasih telah berkontribusi untuk kami")
+    
+    
+bot.start()
+try:
+   print("BOT TELAH AKTIF DAN SIAP DI GUNAKAN")
+   bot.send_messaage(LOG_CHAT, "BOT TELAH AKTIF DAN SIAP DI GUNAKAN")
+   if not str(LOG_CHAT).startswith("-100"):
+       print("Id log anda salah")
+       sys.exit()
+except:
+    pass
